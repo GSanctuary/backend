7eb6170c0348031f654d0a2b935f287daa9f8150
Advay Patil <advaypatil27@gmail.com>
2025-04-27 22:30:37 -0700

    BS migration

---
+++ prisma/migrations/20250428023624_remove_ai_role/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role` on the `AIMessage` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "AIMessage" DROP COLUMN "role";
