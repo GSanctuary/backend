ade463ef6c4c67ab0990fa77e4b9c6bd239aa555
Advay Patil <advaypatil27@gmail.com>
2025-05-19 16:22:08 -0700

    Add route to create task

---
+++ RESTAPI/task/CreateTask.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+import { RESTHandler, RESTMethods, RESTRoute } from '../../server';
+import prisma from '../../lib/prisma';
+
+const schema = z.object({
+  task: z.string().nonempty().max(100),
+});
+
+const handler: RESTHandler = async (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const { task: name } = schema.parse(req.body);
+  const taskInfo = await prisma.task.create({
+    data: {
+      name,
+      userId: req.user.id,
+    },
+  });
+
+  if (!taskInfo) {
+    return res.status(500).json({ error: 'Failed to create task' });
+  }
+
+  res.status(201).json({
+    task: taskInfo,
+  });
+};
+
+const CreateTask: RESTRoute = {
+  method: RESTMethods.POST,
+  needsAuth: true,
+  path: '/task/',
+  schema,
+  run: handler,
+};
+
+export default CreateTask;
