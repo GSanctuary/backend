3271f958a3bac7a6ccdfea7af8949c29f36a12ac
Advay Patil <advaypatil27@gmail.com>
2025-05-20 12:56:13 -0700

    Pagination

--- RESTAPI/task/GetTasks.ts
+++ RESTAPI/task/GetTasks.ts
@@ -6,14 +6,27 @@ const handler: RESTHandler = async (req, res, next) => {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  const tasks = await prisma.task.findMany({
-    where: {
-      userId: req.user.id,
-      completed: false,
-    },
-  });
+  const pageNumber = parseInt(req.query.pageNumber as string) || 1;
+  const pageSize = parseInt(req.query.pageSize as string) || 10;
 
-  return res.status(200).json({ tasks });
+  const [count, tasks] = await prisma.$transaction([
+    prisma.task.count({ where: { userId: req.user.id, completed: false } }),
+    prisma.task.findMany({
+      where: {
+        userId: req.user.id,
+        completed: false,
+      },
+      skip: pageSize * (pageNumber - 1),
+      take: pageSize,
+    }),
+  ]);
+
+  return res.status(200).json({
+    tasks,
+    page: pageNumber,
+    pageCount: Math.ceil(count / pageSize),
+    count,
+  });
 };
 
 const GetTasks: RESTRoute = {
