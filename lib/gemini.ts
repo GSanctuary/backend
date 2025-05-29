023c34760d897dbc42b4e65f3fc39304ef201fbb
Ashvin Ranjan <ranjanashvin@gmail.com>
2025-05-29 12:32:23 -0700

    Fix build errors

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from '@google/genai';
 import { env } from '../env';
 
-const gemini = new GoogleGenAI({ apiKey: env.gemini_key });
+const gemini = new GoogleGenAI({ apiKey: env.GEMINI_API_KEY });
 
 export default gemini;
