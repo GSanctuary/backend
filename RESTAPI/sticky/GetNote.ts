52f5478912795777e602d32cf38a6487ef9adc53
Advay Patil <advaypatil27@gmail.com>
2025-06-03 12:56:33 -0700

    Get all notes instead of just one

--- RESTAPI/sticky/GetNote.ts
+++ RESTAPI/sticky/GetNote.ts
@@ -1,35 +1,18 @@
-import { z } from 'zod';
 import { RESTHandler, RESTMethods, RESTRoute } from '../../server';
 import prisma from '../../lib/prisma';
 
-const schema = z.object({
-  noteId: z.number().int().positive('Note ID must be a positive integer'),
-});
-
-const handler: RESTHandler = async (req, res, next) => {
+const handler: RESTHandler = async (req, res) => {
   if (!req.user) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  // Implement logic here
-  const { noteId } = schema.parse(req.query);
-  const note = await prisma.stickyNote.findUnique({
+  const notes = await prisma.stickyNote.findMany({
     where: {
-      id: noteId,
       userId: req.user.id,
     },
-    select: {
-      id: true,
-      content: true,
-      metadata: true,
-      createdAt: true,
-      updatedAt: true,
-    },
   });
-  if (!note) {
-    return res.status(404).json({ error: 'Note not found' });
-  }
-  return res.status(200).json({ note });
+
+  return res.json({ notes });
 };
 
 const GetNote: RESTRoute = {
@@ -37,7 +20,6 @@ const GetNote: RESTRoute = {
   needsAuth: true,
   path: '/sticky/',
   run: handler,
-  schema,
 };
 
 export default GetNote;
